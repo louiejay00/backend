c898632245409c03fb84ac085801e34dade08585
louiejay00 <louie.jay.becudo.99@gmail.com>
2022-05-25 10:51:10 +0800

    master

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ app.get('/', (req, res) => {
   res.send('Server Running .....')
 });
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+  });
+
 // User Routes
 app.use("/user", userRoutes);
 //Driver Routes
