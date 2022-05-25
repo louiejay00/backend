d6b988cd97421b40fc0a1223799c03a22b88444c
louiejay00 <louie.jay.becudo.99@gmail.com>
2022-05-25 13:49:07 +0800

    master

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ const processRoute = require("./router/process");
 app.use(express.json());
 app.use(cors());
 
-const corsOptions ={
-  origin:'https://etulod-admin-web.herokuapp.com/', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-  next();
-  });
-  
 //DB URL||URI
 const CONNECTION_URL =
   "mongodb+srv://root:root@cluster0.pkf1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
