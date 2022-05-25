cf1be9be747fd054f04d12b1af3e47f6a3f244a8
louiejay00 <louie.jay.becudo.99@gmail.com>
2022-05-25 13:41:45 +0800

    master

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ app.use(express.json());
 app.use(cors());
 
 
-// const corsOptions ={
-//   origin:'https://etulod-admin-web.herokuapp.com/', 
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+  origin:'https://etulod-admin-web.herokuapp.com/', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
-// app.use(function (req, res, next) {
-//   //Enabling CORS
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//   next();
-//   });
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+  });
 
 //DB URL||URI
 const CONNECTION_URL =
